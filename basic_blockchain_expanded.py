0befeb13ce1f8605a6807f2a72000f1eb2038f84
James Strik <jamesstrik@Jamess-MacBook-Pro-3.local>
2024-01-22 14:15:48 -0500

    test

--- basic_blockchain_expanded.py
+++ basic_blockchain_expanded.py
@@ -1,187 +1,98 @@
-from datetime import datetime
+### Create a Blockchain ###
+
+# Import Libraries
+
+import datetime
 import hashlib
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
+### Build Blockchain
 
-class Blockchain:
-    __leading_zeros = 5
-
-    def __init__(self):
+class Blockchain: 
+    
+    def __init__(self): 
         self.chain = []
-        self.transaction_pool = []
-        self.completed_transactions = []
-        self.users = {}
-        self.create_genesis_block()
-
-    def create_genesis_block(self):
-        self.satoshi()
-        nonce, hash, seconds = self.mine()
-        print(f"Genesis block mined in {seconds} seconds.")
-        self.create_block(nonce, hash)
-
-    def satoshi(self):
-        self.create_user('God')
-        self.create_user('Satoshi')
-        self.users['God']['balance'] += 1000
-        transaction = {'sender': 'God',
-                       'receiver': 'Satoshi',
-                       'amount': 1000,
-                       'timestamp': str(datetime.now())}
-        self.transaction_pool.append(transaction)
-
-    def create_block(self, nonce, hash):
+        self.create_block(proof = 1, previous_hash = '0', data = input("data: "))
+        
+    def create_block(self, proof, previous_hash): 
         block = {'index': len(self.chain) + 1,
-                 'timestamp': str(datetime.now()),
-                 'nonce': nonce,
-                 'transactions': self.transaction_pool,
-                 'previous_hash': self.hash(self.get_previous_block()),
-                 'hash': hash
-                 }
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 'data': data}
         self.chain.append(block)
-        self.complete_transactions()
         return block
-
-    def create_user(self, name):
-        self.users[name] = {
-            'balance': 0,
-            'transactions': [],
-        }
-
-    def complete_transactions(self):
-        for t in self.transaction_pool:
-            self.users[t['sender']]['transactions'].append(t)
-            self.users[t['receiver']]['transactions'].append(t)
-            self.users[t['sender']]['balance'] -= t['amount']
-            self.users[t['receiver']]['balance'] += t['amount']
-        self.completed_transactions.append(self.transaction_pool)
-        self.transaction_pool = []
-
-    def get_previous_block(self):
-        if len(self.chain) == 0:
-            return {}
+    
+    def get_previous_block(self): 
         return self.chain[-1]
-
-    def mine(self):
-        start = datetime.now()
-        nonce = 1
-        previous_hash = self.hash(self.get_previous_block())
-        static_hash_input = str(self.transaction_pool) + previous_hash
-        while True:
-            hash_input = f"{nonce}{static_hash_input}"
-            hash = hashlib.sha256(hash_input.encode()).hexdigest()
-            if hash.startswith('0' * self.__leading_zeros):
-                break
-            nonce += 1
-        seconds = (datetime.now() - start).total_seconds()
-        print(f"Block mined in {seconds} seconds.")
-        return nonce, hash, seconds
-
-    def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys=True).encode()
+    
+    def proof_of_work(self, previous_proof): 
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else: 
+                new_proof += 1
+        return new_proof
+    
+    def hash(self, block): 
+        encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-
-    def validate_chain(self):
-        previous_block = self.chain[0]
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
         block_index = 1
-        while block_index < len(self.chain):
-            block = self.chain[block_index]
-            if self.hash(previous_block) != block['previous_hash']:
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
-            hash_input = f"{block['nonce']}{str(block['transactions'])}{self.hash(previous_block)}"
-            hash = hashlib.sha256(hash_input.encode()).hexdigest()
-            if hash[:self.__leading_zeros] != '0' * self.__leading_zeros or hash != block['hash']:
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
 
-    def add_transaction_to_pool(self, sender, receiver, amount):
-        transaction = {'sender': sender,
-                       'receiver': receiver,
-                       'amount': amount,
-                       'timestamp': str(datetime.now())}
-        if not self.validate_transaction(transaction):
-            return False, 'Invalid transaction. Transaction not added to pool.'
-        self.transaction_pool.append(transaction)
-        return True, f"Transaction added to pool and will be included in Block {blockchain.get_previous_block()['index'] + 1}"
+### Mine Blockchain
 
-    def validate_transaction(self, transaction):
-        if transaction['sender'] not in self.users or transaction['receiver'] not in self.users:
-            return False
-        if transaction['amount'] <= 0 or self.users[transaction['sender']]['balance'] < transaction['amount']:
-            return False
-        return True
-
-    def get_user_balances(self):
-        user_balances = {username: user['balance']
-                         for username, user in self.users.items()}
-        return user_balances
-
-
+# Create Web App
 app = Flask(__name__)
 
+# Create Blockchain
 blockchain = Blockchain()
 
-
-@app.route('/mine_block', methods=['GET'])
+# Mine a new block
+@app.route('/mine_block', methods = ['GET'])
 def mine_block():
-    nonce, hash, seconds = blockchain.mine()
-    block = blockchain.create_block(nonce, hash)
-    block['message'] = f"Congratulations, you just mined a block in {seconds} seconds!"
-    return jsonify(block), 200
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
 
-
-@app.route('/get_chain', methods=['GET'])
+# Get a full blockchain
+@app.route('/get_chain', methods = ['GET'])
 def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
-
-@app.route('/confirm_chain', methods=['GET'])
+# Confirm chain is valid
+@app.route('/confirm_chain', methods = ['GET'])
 def confirm_chain():
-    response = {'valid': blockchain.validate_chain()}
+    response = {'valid': blockchain.is_chain_valid(blockchain.chain)}
     return jsonify(response), 200
 
-
-@app.route('/add_transaction', methods=['POST'])
-def add_transaction():
-    values = request.get_json()
-    success, message = blockchain.add_transaction_to_pool(
-        values['sender'], values['receiver'], values['amount'])
-    response = {
-        'transaction added': success,
-        'message': message}
-    return jsonify(response), 201
-
-
-@app.route('/get_transactions', methods=['GET'])
-def get_transactions():
-    response = {'transaction_pool': blockchain.transaction_pool,
-                'completed_transactions': blockchain.completed_transactions}
-    return jsonify(response), 200
-
-
-@app.route('/get_users', methods=['GET'])
-def get_users():
-    return jsonify(blockchain.users), 200
-
-
-@app.route('/get_user_balances', methods=['GET'])
-def get_user_balances():
-    balances = blockchain.get_user_balances()
-    return jsonify(balances), 200
-
-
-@app.route('/create_user', methods=['POST'])
-def create_user():
-    values = request.get_json()
-    blockchain.create_user(values['name'])
-    response = {'message': 'Account creation successful.',
-                'username': values['name'],
-                'status': blockchain.users[values['name']]}
-    return jsonify(response), 201
-
-
-app.run(host='0.0.0.0', port=5000)
+# Run app
+app.run(host = '0.0.0.0', port = 5060)
